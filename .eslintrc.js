7d99784b7a87540a7666e8b3f14c925ac49d5d5c
Benjamin Macher <macher.benjamin@icloud.com>
2021-05-11 11:20:41 +0200

    tooling(eslint): switch off import/prefer-default-export, no-unused-expressions

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,18 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    // Default exports can lead to different names in import
+    // whereas import with destructuring not.
+    'import/prefer-default-export': 'off',
+
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    'no-unused-expressions': [
+      'error',
+      // Allow a && b()
+      { allowShortCircuit: true },
+    ],
   },
   overrides: [
     {
